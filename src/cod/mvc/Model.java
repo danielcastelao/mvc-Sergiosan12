94f95f3cbfbb9daec412759b2eab0ac7f743978e
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-03 09:54:36 +0200

    Creadas clases Coche y Model con sus metodos

---
+++ src/cod/mvc/Model.java
@@ -0,0 +1,40 @@
+package cod.mvc;
+
+import java.util.ArrayList;
+public class Model {
+    private  static ArrayList<Coche> parking;
+
+    public Model(){
+        parking = new ArrayList<Coche>();
+    }
+
+    public static Coche crearCoche(String matricula, String modelo, Integer velocidad){
+        Coche coche = new Coche(matricula, modelo, velocidad);
+        parking.add(coche);
+        return coche;
+    }
+
+    public static Coche getCoche(){
+        return parking.get(0);
+    }
+
+    public static Integer cambiarVelocidad(String matricula, Integer velocidad){
+        for (Coche coche : parking) {
+            if (coche.getMatricula().equals(matricula)) {
+                coche.setVelocidad(velocidad);
+                return velocidad;
+            }
+        }
+        return null;
+    }
+
+    public static Integer getVelocidad(String matricula){
+        for (Coche coche : parking) {
+            if (coche.getMatricula().equals(matricula)) {
+                return coche.getVelocidad();
+            }
+        }
+        return null;
+
+    }
+}
