56e20f5da79b868b78bb3fdfc49984dc4f5df233
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-03 10:04:11 +0200

    Creado test para metodo CrearCoche

---
+++ test/ModelTest.java
@@ -0,0 +1,32 @@
+
+import cod.mvc.Coche;
+import cod.mvc.Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+    private Model model;
+    private String matricula;
+    private String modelo;
+    private Integer velocidad;
+
+    @BeforeEach
+    void setUp() {
+        model = new Model();
+        matricula = "1234ABC";
+        modelo = "ModeloTest";
+        velocidad = 100;
+        model.crearCoche(matricula, modelo, velocidad);
+    }
+
+    @Test
+    void testCrearCoche() {
+        Coche coche = model.crearCoche("5678DEF", "ModeloTest2", 200);
+        assertEquals("5678DEF", coche.getMatricula());
+        assertEquals("ModeloTest2", coche.getModelo());
+        assertEquals(200, coche.getVelocidad());
+    }
+
+
+}
