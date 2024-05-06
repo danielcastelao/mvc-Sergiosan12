28a2144d8b233107c6ace49701f6c8353b06b2d5
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-06 08:59:54 +0200

    Mejora del metodo getCoche

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -4,16 +4,39 @@ import java.util.ArrayList;
 public class Model {
     private static ArrayList<Coche> parking = new ArrayList<>();
 
+    /**
+     *
+     * @param matricula identificador del coche
+     * @param modelo del coche
+     * @param velocidad del coche
+     * @return el coche creado
+     */
     public static Coche crearCoche(String matricula, String modelo, Integer velocidad){
         Coche coche = new Coche(matricula, modelo, velocidad);
         parking.add(coche);
         return coche;
     }
 
-    public static Coche getCoche(){
-        return parking.get(0);
+    /**
+     *
+     * @param matricula
+     * @return coche si lo encuentra, null si no
+     */
+    public static Coche getCoche(String matricula){
+        for (Coche coche : parking){
+            if(coche.getMatricula().equals(matricula)){
+                return coche;
+            }
+        }
+        return null;
     }
 
+    /**
+     * Cambia la velocidad de un coche
+     * @param matricula del coche a cambiar
+     * @param velocidad nueva
+     * @return la velocidad nueva si se ha cambiado, null si no se ha encontrado el coche
+     */
     public static Integer cambiarVelocidad(String matricula, Integer velocidad){
         for (Coche coche : parking) {
             if (coche.getMatricula().equals(matricula)) {
