7a4e4911295b07d26c3d7ed25d71f35ad99d914c
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-10 09:10:32 +0200

    Creado View y sus test

---
+++ src/cod/mvc/View.java
@@ -0,0 +1,8 @@
+package cod.mvc;
+
+public class View {
+    public static boolean muestraVelocidad(String matricula, Integer velocidad){
+        System.out.println("El coche con matrícula " + matricula + " tiene una velocidad de " + velocidad+ " km/h");
+        return true;
+    }
+}
