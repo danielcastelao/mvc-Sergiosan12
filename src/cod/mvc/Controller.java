716cddd45a2955155a47f193b7666fbbb8b39b49
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-10 09:41:37 +0200

    Controller inicial prueba

--- src/cod/mvc/Controller.java
+++ src/cod/mvc/Controller.java
@@ -2,6 +2,16 @@ package cod.mvc;
 
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Creo un coche
+        Model.crearCoche("1234ABC", "ModeloTest1", 100);
+
+        // Cambiar la velocidad del coche
+        Model.cambiarVelocidad("1234ABC", 150);
+
+        // Obtener la velocidad del coche
+        Integer velocidad = Model.getVelocidad("1234ABC");
+
+        // Mostrar la velocidad del coche
+        View.muestraVelocidad("1234ABC", velocidad);
     }
 }
