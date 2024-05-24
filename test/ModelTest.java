2325fb5d4c7410887c22a75f2211c304b5c6241b
Sergio <ssanromangiraldez@danielcastelao.org>
2024-05-24 10:18:09 +0200

    Cambiado el Test del metodo cambiado

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -36,7 +36,7 @@ class ModelTest {
         model.crearCoche("LaFerrari", "1234ABC");
         model.cambiarVelocidad("1234ABC", 180);
         Integer velocidad = model.getVelocidad("1234ABC");
-        assertEquals(170, velocidad);
+        assertEquals(180, velocidad);
     }
 
     @Test
